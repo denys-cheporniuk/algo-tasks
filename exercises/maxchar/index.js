1b0a5ad5feb3f2152edb72ccac3143ebde822120
Denys Cheporniuk <denys.cheporniuk@gmail.com>
2023-10-15 14:21:20 +0300

    implement maxchar task

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charCount = {};
+
+  for (const char of str) {
+    if (char in charCount) {
+      charCount[char] += 1
+    } else {
+      charCount[char] = 1
+    }
+  }
+
+  let max = 0;
+  let maxChar = '';
+
+  for (const key in charCount) {
+    if (charCount[key] > max) {
+      max = charCount[key];
+      maxChar = key;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
