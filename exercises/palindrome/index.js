c0c432761cad9572856cb3719fa104c5a3573fba
Denys Cheporniuk <denys.cheporniuk@gmail.com>
2023-12-06 17:13:34 +0200

    add number of pairs task with solution

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -1,16 +1,47 @@
-// --- Directions
-// Given a string, return true if the string is a palindrome
-// or false if it is not.  Palindromes are strings that
-// form the same word if it is reversed. *Do* include spaces
-// and punctuation in determining if the string is a palindrome.
-// --- Examples:
-//   palindrome("abba") === true
-//   palindrome("abcdefg") === false
+/* Given an array of integers nums, return the number of good pairs.
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
 
-function palindrome(str) {
-  return str.split('').every((char, index) => (
-    char === str[str.length - 1 - index]
-  ))
+Example 1:
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.*/
+
+// FIRST SOLUTION - O(n^2)
+function numIdenticalPairs1(nums) {
+  let pairs = 0;
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j]) {
+        pairs++;
+      }
+    }
+  }
+
+  return pairs;
 }
 
-module.exports = palindrome;
+
+// SECOND SOLUTION O(n)
+function numIdenticalPairs(nums) {
+  let countMap = new Map();
+  let result = 0;
+
+  // Count the occurrences of each number in the array
+  for (let num of nums) {
+    if (countMap.has(num)) {
+      countMap.set(num, countMap.get(num) + 1);
+    } else {
+      countMap.set(num, 1);
+    }
+  }
+
+  // Calculate the number of good pairs for each number
+  for (let count of countMap.values()) {
+    result += (count * (count - 1)) / 2;
+  }
+
+  return result;
+}
+
+module.exports = numIdenticalPairs;
